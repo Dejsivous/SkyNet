edf8b1c025295b4c2d123c0e2f694699005a4ca9
Dejsivous <dave.hopjan@seznam.cz>
2020-11-22 17:44:00 +0100

    basis of decision-making
    Zaklad rozhodovani kdyz robot narazi na prekazku.
    1) měří vzdalenost
    2) otočí se o 90° doleva
    3) zastaví

--- controllers/controllernewtinkerbot/controllernewtinkerbot.m
+++ controllers/controllernewtinkerbot/controllernewtinkerbot.m
@@ -16,30 +16,88 @@ TIME_STEP = 64;
 %  camera = wb_robot_get_device('camera');
 %  wb_camera_enable(camera, TIME_STEP);
 %  motor = wb_robot_get_device('motor');
+speed = 2
 wheel_left_front = wb_robot_get_device('wheel_left_front');
 wheel_left_back = wb_robot_get_device('wheel_left_back');
 wheel_right_front = wb_robot_get_device('wheel_right_front');
 wheel_right_back = wb_robot_get_device('wheel_right_back');
 
 wb_motor_set_position(wheel_left_front, inf);
-wb_motor_set_velocity(wheel_left_front, -1);
+wb_motor_set_velocity(wheel_left_front, -speed);
 wb_motor_set_position(wheel_left_back, inf);
-wb_motor_set_velocity(wheel_left_back, -1);
+wb_motor_set_velocity(wheel_left_back, -speed);
 wb_motor_set_position(wheel_right_front, inf);
-wb_motor_set_velocity(wheel_right_front, 1);
+wb_motor_set_velocity(wheel_right_front, speed);
 wb_motor_set_position(wheel_right_back, inf);
-wb_motor_set_velocity(wheel_right_back, 1);
+wb_motor_set_velocity(wheel_right_back, speed);
+
+%Distance sensor settings
+DS = wb_robot_get_device('ds');
+wb_distance_sensor_enable(DS, TIME_STEP);
 
 %GPS Settings
-Period = 10
-wb_gps_enable('gps', Period);
-wb_gps_get_values('gps')
+GPS = wb_robot_get_device('gps');
+wb_gps_enable(GPS,TIME_STEP);
+
+%Compas Settings
+Compass = wb_robot_get_device('comp');
+wb_compass_enable(Compass, TIME_STEP);
+
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
+m = 0
 while wb_robot_step(TIME_STEP) ~= -1
+distance = wb_distance_sensor_get_value(DS);
+% disp(distance);
+  
+x_y_z = wb_gps_get_values(GPS);
+% disp(x_y_z);
 
+Site = wb_compass_get_values(Compass);
+A = Site(1);
+B = Site(3);
+switch m
+    case 0
+            if distance < 80
+                m = 1
+            end
+%             wb_motor_set_position(wheel_left_back, inf);
+%             wb_motor_set_velocity(wheel_left_back, speed*2);
+%             wb_motor_set_position(wheel_left_front, inf);
+%             wb_motor_set_velocity(wheel_left_front, speed*2);
+%             wb_motor_set_position(wheel_right_back, inf);
+%             wb_motor_set_velocity(wheel_right_back, speed*2);
+%             wb_motor_set_position(wheel_right_front, inf);
+%             wb_motor_set_velocity(wheel_right_front, speed*2);
+%             if Site(1) < 0.5
+%                 m = 1
+%             end
+    case 1
+            wb_motor_set_position(wheel_left_back, inf);
+            wb_motor_set_velocity(wheel_left_back, speed*2);
+            wb_motor_set_position(wheel_left_front, inf);
+            wb_motor_set_velocity(wheel_left_front, speed*2);
+            wb_motor_set_position(wheel_right_back, inf);
+            wb_motor_set_velocity(wheel_right_back, speed*2);
+            wb_motor_set_position(wheel_right_front, inf);
+            wb_motor_set_velocity(wheel_right_front, speed*2);  
+            if A < 0.5
+                m = 2
+            end
+    case 2
+     wb_motor_set_velocity(wheel_left_back,0);
+     wb_motor_set_velocity(wheel_left_front,0);
+     wb_motor_set_velocity(wheel_right_back, 0);
+     wb_motor_set_velocity(wheel_right_front, 0);
+end
+     %wb_motor_set_velocity(wheel_left_back,-2);
+     %wb_motor_set_velocity(wheel_left_front,-2);
+     %wb_motor_set_velocity(wheel_right_back, 2);
+     %wb_motor_set_velocity(wheel_right_front, 2);
+
+  
   % read the sensors, e.g.:
   %  rgb = wb_camera_get_image(camera);
 

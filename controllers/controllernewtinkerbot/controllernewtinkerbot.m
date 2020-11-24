c04add30d79c4ac414cf1fc52d9772e5529a9511
Dejsivous <dave.hopjan@seznam.cz>
2020-11-24 18:15:40 +0100

    Controller with brain
    Robot nyní nenarazí do překážky, která je před ním. Je potřeba upravit pár hodnot u rotace robota, protože ne vždy se otočí přesně do 90°. Zatím se jedná o funkční model mozku.

--- controllers/controllernewtinkerbot/controllernewtinkerbot.m
+++ controllers/controllernewtinkerbot/controllernewtinkerbot.m
@@ -16,7 +16,7 @@ TIME_STEP = 64;
 %  camera = wb_robot_get_device('camera');
 %  wb_camera_enable(camera, TIME_STEP);
 %  motor = wb_robot_get_device('motor');
-speed = 2
+speed = 3
 wheel_left_front = wb_robot_get_device('wheel_left_front');
 wheel_left_back = wb_robot_get_device('wheel_left_back');
 wheel_right_front = wb_robot_get_device('wheel_right_front');
@@ -47,7 +47,12 @@ wb_compass_enable(Compass, TIME_STEP);
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
-m = 0
+N = 0;
+S = 0;
+E = 0;
+W = 0;
+site = 0;
+brain = 0;
 while wb_robot_step(TIME_STEP) ~= -1
 distance = wb_distance_sensor_get_value(DS);
 % disp(distance);
@@ -58,46 +63,151 @@ x_y_z = wb_gps_get_values(GPS);
 Site = wb_compass_get_values(Compass);
 A = Site(1);
 B = Site(3);
-switch m
-    case 0
-            if distance < 80
-                m = 1
-            end
-%             wb_motor_set_position(wheel_left_back, inf);
-%             wb_motor_set_velocity(wheel_left_back, speed*2);
-%             wb_motor_set_position(wheel_left_front, inf);
-%             wb_motor_set_velocity(wheel_left_front, speed*2);
-%             wb_motor_set_position(wheel_right_back, inf);
-%             wb_motor_set_velocity(wheel_right_back, speed*2);
-%             wb_motor_set_position(wheel_right_front, inf);
-%             wb_motor_set_velocity(wheel_right_front, speed*2);
-%             if Site(1) < 0.5
-%                 m = 1
-%             end
-    case 1
-            wb_motor_set_position(wheel_left_back, inf);
-            wb_motor_set_velocity(wheel_left_back, speed*2);
-            wb_motor_set_position(wheel_left_front, inf);
-            wb_motor_set_velocity(wheel_left_front, speed*2);
-            wb_motor_set_position(wheel_right_back, inf);
-            wb_motor_set_velocity(wheel_right_back, speed*2);
-            wb_motor_set_position(wheel_right_front, inf);
-            wb_motor_set_velocity(wheel_right_front, speed*2);  
-            if A < 0.5
-                m = 2
-            end
-    case 2
-     wb_motor_set_velocity(wheel_left_back,0);
-     wb_motor_set_velocity(wheel_left_front,0);
-     wb_motor_set_velocity(wheel_right_back, 0);
-     wb_motor_set_velocity(wheel_right_front, 0);
-end
-     %wb_motor_set_velocity(wheel_left_back,-2);
-     %wb_motor_set_velocity(wheel_left_front,-2);
-     %wb_motor_set_velocity(wheel_right_back, 2);
-     %wb_motor_set_velocity(wheel_right_front, 2);
 
-  
+%Definice sv. stran
+%Definice severu
+if A > 0.979 & B < 0.021
+    N = 1;
+else
+    N = 0;
+end
+%Definice jihu    
+if A < -0.979 & B < 0.021
+    S = 1;
+else
+    S = 0;
+end
+%Definice východu
+if A < 0.021 & B < -0.979
+    E = 1;
+else
+    E = 0;
+end
+%Definice západu
+if A < 0.021 & B > 0.979    
+    W = 1;
+else
+    W = 0;
+end
+% Mozek
+switch brain
+    case 0
+            if distance <= 80
+                brain = 1
+            else
+            wb_motor_set_velocity(wheel_left_back, -speed);
+            wb_motor_set_velocity(wheel_left_front, -speed);
+            wb_motor_set_velocity(wheel_right_back, speed);
+            wb_motor_set_velocity(wheel_right_front, speed); 
+            end        
+    case 1
+        if N == 1 | S == 1
+            brain = 2
+        else
+            brain = 5
+        end        
+    case 2 %Horizontalni rotace
+        wb_motor_set_velocity(wheel_left_back, 1);
+        wb_motor_set_velocity(wheel_left_front, 1);
+        wb_motor_set_velocity(wheel_right_back, 1);
+        wb_motor_set_velocity(wheel_right_front, 1);  
+                if A < 0.2
+                     wb_motor_set_velocity(wheel_left_back, 0.5);
+                     wb_motor_set_velocity(wheel_left_front, 0.5);
+                     wb_motor_set_velocity(wheel_right_back, 0.5);
+                     wb_motor_set_velocity(wheel_right_front, 0.5);              
+                    if W == 1
+                        if distance <= 80
+                            brain = 3
+                        else
+                            brain = 0
+                        end
+                    end
+                end
+    case 3                   
+        wb_motor_set_velocity(wheel_left_back, -1);
+        wb_motor_set_velocity(wheel_left_front, -1);
+        wb_motor_set_velocity(wheel_right_back, -1);
+        wb_motor_set_velocity(wheel_right_front, -1);  
+        if B < -0.8
+            wb_motor_set_velocity(wheel_left_back, -0.5);
+            wb_motor_set_velocity(wheel_left_front, -0.5);
+            wb_motor_set_velocity(wheel_right_back, -0.5);
+            wb_motor_set_velocity(wheel_right_front, -0.5);
+            if E == 1
+                if distance <= 80
+                    brain = 4
+                else
+                    brain = 0
+                end
+            end
+        end
+    case 4
+        wb_motor_set_velocity(wheel_left_back, -1);
+        wb_motor_set_velocity(wheel_left_front, -1);
+        wb_motor_set_velocity(wheel_right_back, -1);
+        wb_motor_set_velocity(wheel_right_front, -1);  
+        if A < -0.8
+            wb_motor_set_velocity(wheel_left_back, -0.5);
+            wb_motor_set_velocity(wheel_left_front, -0.5);
+            wb_motor_set_velocity(wheel_right_back, -0.5);
+            wb_motor_set_velocity(wheel_right_front, -0.5);
+                if S == 1
+                    brain = 0
+                end
+        end
+    case 5 %Vertikalni rotace
+            wb_motor_set_velocity(wheel_left_back, 1);
+            wb_motor_set_velocity(wheel_left_front, 1);
+            wb_motor_set_velocity(wheel_right_back, 1);
+            wb_motor_set_velocity(wheel_right_front, 1);  
+            if A > 0.8
+                 wb_motor_set_velocity(wheel_left_back, 0.5);
+                 wb_motor_set_velocity(wheel_left_front, 0.5);
+                 wb_motor_set_velocity(wheel_right_back, 0.5);
+                 wb_motor_set_velocity(wheel_right_front, 0.5);              
+                if N == 1
+                    if distance <= 80
+                        brain = 6
+                    else 
+                        brain = 0
+                    end
+                end
+            end
+    case 6
+        wb_motor_set_velocity(wheel_left_back, -1);
+        wb_motor_set_velocity(wheel_left_front, -1);
+        wb_motor_set_velocity(wheel_right_back, -1);
+        wb_motor_set_velocity(wheel_right_front, -1);  
+        if A < -0.8
+            wb_motor_set_velocity(wheel_left_back, -0.5);
+            wb_motor_set_velocity(wheel_left_front, -0.5);
+            wb_motor_set_velocity(wheel_right_back, -0.5);
+            wb_motor_set_velocity(wheel_right_front, -0.5);
+                if S == 1
+                    if distance <= 80
+                        brain = 7
+                    else
+                        brain = 0
+                    end
+                end
+        end
+    case 7
+                        
+        wb_motor_set_velocity(wheel_left_back, -1);
+        wb_motor_set_velocity(wheel_left_front, -1);
+        wb_motor_set_velocity(wheel_right_back, -1);
+        wb_motor_set_velocity(wheel_right_front, -1);  
+            if A > -0.2
+                wb_motor_set_velocity(wheel_left_back, -0.5);
+                wb_motor_set_velocity(wheel_left_front, -0.5);
+                wb_motor_set_velocity(wheel_right_back, -0.5);
+                wb_motor_set_velocity(wheel_right_front, -0.5);
+                    if W == 1
+                        brain = 0
+                    end
+            end
+end
   % read the sensors, e.g.:
   %  rgb = wb_camera_get_image(camera);
 
